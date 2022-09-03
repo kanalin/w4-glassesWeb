df2327f3c81b69036b113fa08bbb0a4e9e7adb55
nina <kana202206@gmail.com>
2022-09-03 22:44:25 +0800

    Update 2022-09-03T14:44:17.351Z

---
+++ assets/js/all.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log('Hello!');
+$(document).ready(function () {
+  console.log('HesSchool Hello!');
+});
+$('.ham-icon').click(function (e) {
+  e.preventDefault();
+  $('.dropdown').toggleClass('active');
+});
+//# sourceMappingURL=all.js.map
